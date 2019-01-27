9a9fc8ca1c19284f5f0a0b810811a1eafca5c485
MAKKENAMALLIKARJUNARAO9 <45346947+MAKKENAMALLIKARJUNARAO9@users.noreply.github.com>
2019-01-27 18:13:58 +0530

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,26 @@
+<php
+	$a=$_GET["aval"];
+	$b=$_GET["bval"];
+	$c=$_GET["cval"];
+	$d=$_GET["dval"];
+	$e=$_GET["eval"];
+	$f=$_GET["fval"];
+	$g=$_GET["gval"];
+	$h=$_GET["hval"];
+	$i=$_GET["ival"];
+	$j=$_GET["jval"];
+	$k=$_GET["kval"];
+	$l=$_GET["lval"];
+	echo "Name:$a";
+	echo "Gender:$b";
+	echo "Gender:$c";
+	echo "Address:$d";
+	echo "phonenumber:$e";
+	echo "paymentoption:$f";
+	echo "paymentoption:$g";
+	echo "paymentoption:$h";
+	echo "$i";
+	echo "$j";
+	echo "$k";
+	echo "$l";
+?>
